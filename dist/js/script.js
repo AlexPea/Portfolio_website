dda0e1b99f5f9a58830fef7208e3f933fc7bf89b
Alex <alex.gorokhov.work@gmail.com>
2020-09-01 16:27:32 +0300

    second commit

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,13 +1,18 @@
 // Adds and removes classes from element
 const hamburger = document.querySelector(".hamburger"),
   menu = document.querySelector(".menu"),
-  closeElem = document.querySelector(".menu__close");
+  closeElem = document.querySelector(".menu__close"),
+  overlayElem = document.querySelector(".menu__overlay");
 
 hamburger.addEventListener("click", () => {
   menu.classList.add("active");
 });
 
 closeElem.addEventListener("click", () => {
+  menu.classList.remove("active");
+});
+
+overlayElem.addEventListener("click", () => {
   menu.classList.remove("active");
 });
 
